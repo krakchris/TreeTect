f11e4b015b12597fbe0ad7e51421e74b2cb1421f
Ubuntu <ubuntu@ip-172-31-54-57.ec2.internal>
2020-06-17 07:58:21 +0000

    .

--- ms_model_training/src/training_wrapper.py
+++ ms_model_training/src/training_wrapper.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
         logging.info(f'Downloading {CONFIG_FILE_S3_PATH}')
         print(f'Downloading {CONFIG_FILE_S3_PATH}...')
 
-        s3_data_transfer(CONFIG_FILE_S3_PATH, TRAINING_CONFIG_JSON_PATH, False)    
+        s3_data_transfer('s3://' + CONFIG_FILE_S3_PATH, TRAINING_CONFIG_JSON_PATH, False)    
 
         # --------------------------------loading json file-----------------------------------------
 
@@ -184,7 +184,18 @@ if __name__ == "__main__":
                                    meta_data_json['transfer_learn_from']),
             model_files_dir,
             True)
+            
+            checkpoint_txt_log_path = os.path.join(model_files_dir, 'training', 'checkpoint')
 
+            with open(checkpoint_txt_log_path, 'r') as f:
+                file_data = f.read()
+
+            file_data = file_data.replace(meta_data_json['transfer_learn_from'], model_files_dir.split('/')[-1])
+            
+            print(file_data)
+            with open(checkpoint_txt_log_path, 'w') as f:
+                f.write(file_data)
+                
             shutil.rmtree(os.path.join(model_files_dir, 'eval'))
             shutil.rmtree(os.path.join(model_files_dir, 'output_inference_graph'))
 
