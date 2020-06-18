21d0c7b932614038180249d00d374ed88bf9b86d
sumain <sumain.sharma@daffodilsw.com>
2020-06-18 13:39:29 +0530

    add graph,  and resultant data

--- ms_point_data_comparison/src/point_data_comparison.py
+++ ms_point_data_comparison/src/point_data_comparison.py
@@ -13,6 +13,7 @@
         - Line shape files from actual to predicted point for the nearest tree
 """
 
+import math
 import os
 import sys
 import shutil
@@ -21,6 +22,7 @@ import argparse
 import fiona
 import geopandas as gpd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from shapely.geometry import LineString, box, mapping
 from tqdm import tqdm
@@ -31,6 +33,7 @@ CRS = 'EPSG:32631'
 TEMP_DIR_PATH = '../temp'
 ACTUAL_SHAPE_FILE_LOCAL_PATH = os.path.join(TEMP_DIR_PATH, 'actual.shp')
 PREDICTED_SHAPE_FILE_LOCAL_PATH = os.path.join(TEMP_DIR_PATH, 'predicted.shp')
+GRAPH_RESCALE_FACTOR = 1000
 
 
 def arguments():
@@ -116,6 +119,12 @@ if __name__ == "__main__":
     total_dis_bw_two_pts = 0.0
     result_line_obj_list = []
     total_predicted_points = len(predicted_data_np_array)
+    x_coor_list = []
+    y_coor_list = []
+    quad_1 = 0
+    quad_2 = 0
+    quad_3 = 0
+    quad_4 = 0
 
     for actual_point in tqdm(actual_data_np_array, desc='Processing', file=sys.stdout):
 
@@ -131,6 +140,20 @@ if __name__ == "__main__":
 
             result_line_obj_list.append(LineString([actual_point,
                                                     predicted_point]))
+            # storing x, y points in a list to draw graphs
+            x = (predicted_point[0] - actual_point[0]) * GRAPH_RESCALE_FACTOR
+            y = (predicted_point[1] - actual_point[1]) * GRAPH_RESCALE_FACTOR
+            x_coor_list.append(x)
+            y_coor_list.append(y)
+
+            if x > 0  and y > 0:
+                quad_1 += 1
+            elif x < 0 and y > 0:
+                quad_2 += 1
+            elif x < 0 and y < 0:
+                quad_3 += 1
+            elif x > 0 and y < 0:
+                quad_4 += 1 
 
             predicted_data_np_array = np.delete(predicted_data_np_array, min_dis_index, axis=0)
 
@@ -144,7 +167,14 @@ if __name__ == "__main__":
         res_file_obj.write(f'Average distance  : {len(result_line_obj_list)}\n')
         res_file_obj.write(f'Total actual points : {total_dis_bw_two_pts/len(result_line_obj_list)}\n')
         res_file_obj.write(f'Accuracy : {(len(result_line_obj_list)/ float(len(actual_data_np_array)))*100}\n')
+        res_file_obj.write(f'Total points in quadrant 1 : {quad_1}\n')
+        res_file_obj.write(f'Total points in quadrant 2 : {quad_2}\n')
+        res_file_obj.write(f'Total points in quadrant 3 : {quad_3}\n')
+        res_file_obj.write(f'Total points in quadrant 4 : {quad_4}\n')
+        res_file_obj.write(f'Resultant angle : {math.degrees(math.atan(sum(x_coor_list)/sum(y_coor_list)))}\n')
+        res_file_obj.write(f'Resultant magnitude : {math.sqrt(sum(x_coor_list)**2 + sum(y_coor_list)**2)/GRAPH_RESCALE_FACTOR}')
 
+    print('Creating shape file...')
     schema = {
         'geometry': 'LineString',
         'properties': {}
@@ -159,5 +189,11 @@ if __name__ == "__main__":
             c.write({
                 'geometry': mapping(line_obj),
                 'properties': {}})
+
+    print('Generation plot image...')
+    plt.figure(figsize=(20,20))
+    plt.plot([[0]*len(x_coor_list), x_coor_list],
+            [[0]*len(y_coor_list), y_coor_list]) 
+    plt.savefig(os.path.join(args['output_dir'], 'res.png'))
 
     shutil.rmtree(TEMP_DIR_PATH)
