440e257837fe1fa84ffbac5bc3393e806c45f9d0
sumain <sumain.sharma@daffodilsw.com>
2020-06-22 16:18:12 +0530

    lambda and docker code update

--- ms_file_chunker/lambda_function.py
+++ ms_file_chunker/lambda_function.py
@@ -1,26 +1,44 @@
-import json
-import logging
-import os
-import time
-import uuid
-from datetime import datetime
+'''
+    Lambda function to which get triggered on event from s3
+    this will get filename from event and run fargate task by updating env to fargate service.
+'''
+
 import boto3
 
-# config
 REGION = 'us-east-1'
 FARGATE_CLUSTER = 'techtree'
-FARGATE_TASK_DEF_NAME = 'Filechunker:2'
+FARGATE_TASK_DEF_NAME = 'Filechunker3'
 FARGATE_SUBNET_ID = 'subnet-69b85848'
 CONTAINER_NAME = 'filechunker'
 
+S3_LOG_FILE_UPLOAD_PATH = 'treetech-workflow/Logs/Filechunker'
+
 def lambda_handler(event, context):
-    
+
+    # get event data
+    event_record = event['Records']
+    bucket_data = event_record[0]
+    bucket_name = bucket_data['s3']['bucket']['name']
+    file_name = bucket_data['s3']['object']['key']
+
+    src_file_path = bucket_name + "/" + file_name
+    dst_dir_path = ('/'.join(src_file_path.split('/')[:-1])).replace('source_data',
+                                                                     'chunked_data',
+                                                                     1)
+
+    # logs
+    print('Event', event_record)
+    print('bucket_data=', bucket_data)
+    print('src file path =', src_file_path)
+    print('dst_dir_path =', dst_dir_path)
+
+    # set env and run task using fargate service
     client = boto3.client('ecs', region_name=REGION)
     resp = client.run_task(
         cluster=FARGATE_CLUSTER,
-        launchType = 'FARGATE',
+        launchType='FARGATE',
         taskDefinition=FARGATE_TASK_DEF_NAME,
-        count = 1,
+        count=1,
         platformVersion='LATEST',
         networkConfiguration={
             'awsvpcConfiguration': {
@@ -37,22 +55,22 @@ def lambda_handler(event, context):
                     'environment': [
                         {
                             'name': 'S3_BIG_TIF_FILE_PATH',
-                            'value': 'gcw-treetect-common-input-data-dev/input_data/source_data/worldview/Amsterdam/modified_merged.tif'
+                            'value': src_file_path
                         },
                         {
                             'name': 'S3_CHUNKED_TIF_DIR_PATH',
-                            'value': 'gcw-treetect-tree-detection-dev/temp_data/chunked_tif_data'
+                            'value': dst_dir_path
                         },
                         {
                             'name': 'S3_LOG_FILE_UPLOAD_PATH',
-                            'value': 'gcw-treetect-tree-detection-dev/training_logs'
+                            'value': S3_LOG_FILE_UPLOAD_PATH
                         },
                     ],
                 },
             ],
         },
     )
-
+    print('Fargate run task response:', str(resp))
     response = {
         "statusCode": 200,
         "body": str(resp)
